074024936f81c7b1077ea3e464dda0c0272d7f64
Ciiruu <ricardho.gunawan@gmail.com>
2022-10-04 11:51:36 +0700

    create belajar.java

---
+++ belajar.java
@@ -0,0 +1,18 @@
+package PBO.OOP;
+
+import java.util.Scanner;
+
+public class belajar {
+    public static void main(String[] args) {
+        // ===================ARRAy===================
+
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("masukan panjang array");
+        // // int panjangarray = scan.nextInt();
+        // // int[] angka = new int[panjangarray];
+
+        // int[][] angka2 = new int[2][2];
+        // int[][] angka2_2 = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
+        // System.out.println(angka2_2);
+    }
+}
